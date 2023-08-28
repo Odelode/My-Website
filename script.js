2251f6a93ac7da1f4b5da5cde6b4132757495c70
Odelode <110647444+Odelode@users.noreply.github.com>
2023-08-28 23:52:32 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,28 @@
-
 window.addEventListener('load', function() {
-
     setTimeout(function() {
         document.body.style.opacity = 1; 
         document.body.style.transform = 'translateX(0)'; 
     }, 300); 
 });
 
-
 function toggleDarkMode() {
     const body = document.body;
     body.classList.toggle('dark-mode');
 
     localStorage.setItem('darkModeEnabled', body.classList.contains('dark-mode'));
+
+    updateToggleLabel(); // Update the button text when the mode is toggled
 }
 
 const darkModeToggle = document.getElementById('darkModeToggle');
-darkModeToggle.addEventListener('change', toggleDarkMode);
+darkModeToggle.addEventListener('click', toggleDarkMode); // Listen for the click event
 
-if (localStorage.getItem('darkModeEnabled') === 'true')
+if (localStorage.getItem('darkModeEnabled') === 'true') {
     document.body.classList.add('dark-mode');
-
+    updateToggleLabel(); // Update the button text when starting in dark mode
+}
 
 function updateToggleLabel() {
     const darkModeLabel = document.querySelector('.dark-mode-toggle label');
     darkModeLabel.textContent = document.body.classList.contains('dark-mode') ? 'Light Mode' : 'Dark Mode';
 }
-
-updateToggleLabel();
-
-document.body.addEventListener('classChange', updateToggleLabel);
-
