291c071c18c6ffd8e6d4a2745f852b995202659c
Odelode <110647444+Odelode@users.noreply.github.com>
2023-10-05 14:38:03 +0200

    added a video gallery

--- nav-component.js
+++ nav-component.js
@@ -10,6 +10,7 @@ class NavComponent extends HTMLElement {
                     <li><a href="index.html">Home Page</a></li>
                     <li><a href="hobbies.html">My Hobbies</a></li>
                     <li><a href="gallery.html">Gallery</a></li>
+                    <li><a href="video-gallery.html">Video Gallery</a></li>
                     <li><a href="credits.html">Sources</a></li>
                 </ul>
             </nav>
