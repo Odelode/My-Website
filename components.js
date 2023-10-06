4be18a68585aaaf89a06d440df0bed0d57c1f1b0
Odelode <110647444+Odelode@users.noreply.github.com>
2023-10-06 13:55:55 +0200

    Just added some content on one of the html files

---
+++ components.js
@@ -0,0 +1,21 @@
+class NavComponent extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
+            <nav>
+                <ul>
+                    <li><a href="index.html">Home Page</a></li>
+                    <li><a href="hobbies.html">My Hobbies</a></li>
+                    <li><a href="gallery.html">Gallery</a></li>
+                    <li><a href="video-gallery.html">Video Gallery</a></li>
+                    <li><a href="credits.html">Sources</a></li>
+                </ul>
+            </nav>
+        `;
+    }
+}
+
+customElements.define('nav-component', NavComponent);
