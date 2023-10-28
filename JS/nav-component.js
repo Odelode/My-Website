663a2c3e126e697757f773b44b236ce18f82e3ce
Odelode <110647444+Odelode@users.noreply.github.com>
2023-10-28 15:25:22 +0200

    Just removed something that is still a work in progress

--- JS/nav-component.js
+++ JS/nav-component.js
@@ -12,7 +12,6 @@ class NavComponent extends HTMLElement {
                     <li><a href="gallery.html">Gallery</a></li>
                     <li><a href="video-gallery.html">Video Gallery</a></li>
                     <li><a href="music.html">Music likes</a></li>
-                    <li><a href="Reviews/reviews.html">Reviews</a></li>
                     <li><a href="awards.html">Awards</a></li>
                     <li><a href="credits.html">Sources</a></li>
                 </ul>
