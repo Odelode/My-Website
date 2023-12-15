d0b7bfe397804779e9cd3bcf4c7ce52be2f5d95f
BuildTools <unconfigured@null.spigotmc.org>
2023-12-15 02:04:44 +0100

    Added Dark Mode

---
+++ JS/dark-mode-style.js
@@ -0,0 +1,50 @@
+const toggleButton = document.getElementById('darkModeToggle');
+const darkModeStylesheets = ['../CSS/dark-mode-styles.css'];
+
+toggleButton.addEventListener('click', function() {
+  const isDarkMode = localStorage.getItem('darkMode') === 'true';
+
+  if (isDarkMode) {
+    localStorage.setItem('darkMode', 'false');
+    removeDarkModeStylesheets();
+    this.textContent = 'Toggle Dark Mode';
+  } else {
+    localStorage.setItem('darkMode', 'true');
+    addDarkModeStylesheets();
+    this.textContent = 'Toggle Light Mode';
+  }
+});
+
+window.onload = function() {
+  const isDarkMode = localStorage.getItem('darkMode') === 'true';
+  if (isDarkMode) {
+    addDarkModeStylesheets();
+    toggleButton.textContent = 'Toggle Light Mode';
+  } else {
+    toggleButton.textContent = 'Toggle Dark Mode';
+  }
+};
+
+function addDarkModeStylesheets() {
+  darkModeStylesheets.forEach((stylesheet) => {
+    const linkElement = document.createElement('link');
+    linkElement.rel = 'stylesheet';
+    linkElement.href = stylesheet;
+    linkElement.id = 'darkModeStylesheet';
+    document.head.appendChild(linkElement);
+  });
+}
+
+function removeDarkModeStylesheets() {
+  darkModeStylesheets.forEach((stylesheet) => {
+    const linkElement = document.querySelector(`link[href="${stylesheet}"]`);
+    linkElement && linkElement.parentNode.removeChild(linkElement);
+  });
+}
+
+window.onload = function() {
+  const isDarkMode = localStorage.getItem('darkMode') === 'true';
+  if (isDarkMode) {
+    addDarkModeStylesheets();
+  }
+};
