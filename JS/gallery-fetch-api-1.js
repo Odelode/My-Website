ff64e36061633a49c96900dc1263e0c962ba5e4a
Odelode <odinlode@gmail.com>
2024-01-05 09:02:27 +0100

    I put code that was in 3 seperate files into 1 file

--- JS/gallery-fetch-api-1.js
+++ JS/gallery-fetch-api-1.js
@@ -16,3 +16,41 @@ fetch('../JSON/gallery-1.json')
     });
   })
   .catch(error => console.error('Error:', error));
+
+  fetch('../JSON/gallery-2.json')
+  .then(response => response.json())
+  .then(data => {
+    const gallery = document.querySelector('.gallery-2');
+    data.forEach(item => {
+      let img = document.createElement('img');
+      img.setAttribute('loading', 'lazy')
+      img.src = item.url;
+      img.alt = item.alt;
+
+      let div = document.createElement('div');
+      div.classList.add('gallery-item');
+      div.appendChild(img);
+
+      gallery.appendChild(div);
+    });
+  })
+  .catch(error => console.error('Error:', error));
+
+  fetch('../JSON/gallery-3.json')
+  .then(response => response.json())
+  .then(data => {
+    const gallery = document.querySelector('.gallery-3');
+    data.forEach(item => {
+      let img = document.createElement('img');
+      img.setAttribute('loading', 'lazy')
+      img.src = item.url;
+      img.alt = item.alt;
+
+      let div = document.createElement('div');
+      div.classList.add('gallery-item');
+      div.appendChild(img);
+
+      gallery.appendChild(div);
+    });
+  })
+  .catch(error => console.error('Error:', error));
