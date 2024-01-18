8f5e1384457ecca563a9287fb01bc129edf85651
Odelode <odinlode@gmail.com>
2024-01-18 09:42:07 +0100

    I made another fetch api for something that did not need it

---
+++ JS/hobbies-fetch-api.js
@@ -0,0 +1,84 @@
+fetch('../JSON/hobbies-1.json')
+  .then(response => response.json())
+  .then(data => {
+    const hobbies = document.querySelector('.links-1');
+    data.forEach(item => {
+      let a = document.createElement('a');
+      a.setAttribute('target', '_blank')
+      a.href = item.url;
+      a.text = item.alt;
+
+      let li = document.createElement('li');
+      li.appendChild(a);
+
+      hobbies.appendChild(li);
+    });
+  })
+
+  fetch('../JSON/hobbies-2.json')
+  .then(response => response.json())
+  .then(data => {
+    const hobbies = document.querySelector('.links-2');
+    data.forEach(item => {
+      let a = document.createElement('a');
+      a.setAttribute('target', '_blank')
+      a.href = item.url;
+      a.text = item.alt;
+
+      let li = document.createElement('li');
+      li.appendChild(a);
+
+      hobbies.appendChild(li);
+    });
+  })
+
+  fetch('../JSON/hobbies-3.json')
+  .then(response => response.json())
+  .then(data => {
+    const hobbies = document.querySelector('.links-3');
+    data.forEach(item => {
+      let a = document.createElement('a');
+      a.setAttribute('target', '_blank')
+      a.href = item.url;
+      a.text = item.alt;
+
+      let li = document.createElement('li');
+      li.appendChild(a);
+
+      hobbies.appendChild(li);
+    });
+  })
+
+  fetch('../JSON/hobbies-4.json')
+  .then(response => response.json())
+  .then(data => {
+    const hobbies = document.querySelector('.links-4');
+    data.forEach(item => {
+      let a = document.createElement('a');
+      a.setAttribute('target', '_blank')
+      a.href = item.url;
+      a.text = item.alt;
+
+      let li = document.createElement('li');
+      li.appendChild(a);
+
+      hobbies.appendChild(li);
+    });
+  })
+
+  fetch('../JSON/hobbies-5.json')
+  .then(response => response.json())
+  .then(data => {
+    const hobbies = document.querySelector('.links-5');
+    data.forEach(item => {
+      let a = document.createElement('a');
+      a.setAttribute('target', '_blank')
+      a.href = item.url;
+      a.text = item.alt;
+
+      let li = document.createElement('li');
+      li.appendChild(a);
+
+      hobbies.appendChild(li);
+    });
+  })
