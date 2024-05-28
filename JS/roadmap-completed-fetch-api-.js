8a701b853351887d5a3693379f5e19df37b6889c
Odelode <odinlode@gmail.com>
2024-05-28 10:55:02 +0200

    tehe taha

--- JS/roadmap-completed-fetch-api-.js
+++ JS/roadmap-completed-fetch-api-.js
@@ -1,4 +1,4 @@
-fetch('../JSON/roadmap-not-completed.json')
+fetch('../JSON/roadmap-completed.json')
   .then(response => response.json())
   .then(data => {
     const roadmap = document.querySelector('.roadmap');
@@ -10,10 +10,10 @@ fetch('../JSON/roadmap-not-completed.json')
 
       let div = document.createElement('div');
       div.classList.add('milestone');
+      div.classList.add('completed');
       div.appendChild(h2);
       div.appendChild(p);
 
       roadmap.appendChild(div);
     });
 })
-  
